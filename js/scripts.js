a6263682a20d418c3a2976797cdbab08aac461ed
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-10-16 18:08:54 -0400

    Add event handler for form input

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,40 @@
 const myLibrary = [];
+const addBookBtn = document.getElementById('add-book-submit');
 
-function Book(title, author, publishDate) {
+addBookBtn.addEventListener('click', () => {
+    addBookFormSubmit();
+});
+
+function addBookFormSubmit() {
+    let form = document.getElementById('add-book-form');
+    let inputs = form.querySelectorAll('input');
+    let formObj = {};
+    inputs.forEach(input => {
+        let key = input.id;
+        formObj[key] = input.value;
+    });
+
+    addBookToLibrary(formObj.title, formObj.author, formObj.publishdate, formObj.pagecount, formObj.readstatus);
+    outputBooks();
+}
+
+function Book(title, author, publishDate, pagecount, readstatus) {
     this.title = title;
     this.author = author;
     this.publishDate = publishDate;
+    this.pagecount = pagecount;
+    this.readstatus = readstatus;
 }
 
-function addBookToLibrary(title, author, publishDate) {
-    let book = new Book(title, author, publishDate);
+function addBookToLibrary(title, author, publishDate, pagecount, readstatus) {
+    let book = new Book(title, author, publishDate, pagecount, readstatus);
     myLibrary.push(book);
 }
 
 function outputBooks() {
+    let library = document.getElementById('library');
+    library.innerHTML = '';
     myLibrary.forEach(book => {
-        let library = document.getElementById('library');
         library.appendChild(bookCard(book));
     });
 }
@@ -33,12 +54,12 @@ function bookCard(book) {
     card.appendChild(author);
     card.appendChild(date);
     
-    card.style.background = `linear-gradient(90deg, #ffffff, #ffffff), linear-gradient(189deg, rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5), rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.75)`;
+    card.style.background = `linear-gradient(90deg, #ffffff, #ffffff), linear-gradient(45deg, rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5), rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.75)`;
 
     return card;
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '1937');
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '1937');
 addBookToLibrary('Watership Down', 'Richard Adams', '1972');
-addBookToLibrary('Dune', 'Frank Herber', '1965');
+addBookToLibrary('Dune', 'Frank Herbert', '1965');
 outputBooks();
