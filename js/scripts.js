20c853ac90875237e1466cc8eb7e5ce41bf37c64
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-10-17 06:38:13 -0400

    Add delete book button

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 const myLibrary = [];
+const library = document.getElementById('library');
 const addBookBtn = document.getElementById('add-book-submit');
+let bookID = 0;
 
 addBookBtn.addEventListener('click', () => {
     addBookFormSubmit();
 });
+
+library.addEventListener('click', (e) => {
+    if (e.target.innerText === 'X') {
+        deleteBook(e.target.dataset.id);
+    }
+});
+
+function deleteBook(id) {
+    for (let i=0; i<myLibrary.length; i++) {
+        if (myLibrary[i].id == id) {
+            myLibrary.splice(i, 1);
+            break;
+        }
+    }
+    outputBooks();
+}
+
 
 function addBookFormSubmit() {
     let form = document.getElementById('add-book-form');
@@ -18,33 +37,41 @@ function addBookFormSubmit() {
     outputBooks();
 }
 
-function Book(title, author, publishDate, pagecount, readstatus) {
+function Book(title, author, publishDate, pagecount, readstatus, id) {
     this.title = title;
     this.author = author;
     this.publishDate = publishDate;
     this.pagecount = pagecount;
     this.readstatus = readstatus;
+    this.id = id;
+    this.style = `linear-gradient(90deg, #ffffff, #ffffff), linear-gradient(45deg, rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5), rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.75)`;
 }
 
 function addBookToLibrary(title, author, publishDate, pagecount, readstatus) {
-    let book = new Book(title, author, publishDate, pagecount, readstatus);
+    let book = new Book(title, author, publishDate, pagecount, readstatus, bookID);
+    bookID++;
     myLibrary.push(book);
 }
 
 function outputBooks() {
-    let library = document.getElementById('library');
     library.innerHTML = '';
+ 
     myLibrary.forEach(book => {
         library.appendChild(bookCard(book));
     });
+
 }
 
 function bookCard(book) {
-    card = document.createElement('div');
+    let card = document.createElement('div');
     card.classList.add('book-card');
-    title = document.createElement('h2');
-    author = document.createElement('h3');
-    date = document.createElement('span');
+    card.dataset.id = book.id;
+    let title = document.createElement('h2');
+    let author = document.createElement('h3');
+    let date = document.createElement('span');
+    let closeBtn = document.createElement('button');
+    closeBtn.dataset.id = book.id;
+    closeBtn.textContent = 'X';
 
     title.textContent = book.title;
     author.textContent = book.author;
@@ -53,13 +80,10 @@ function bookCard(book) {
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(date);
-    
-    card.style.background = `linear-gradient(90deg, #ffffff, #ffffff), linear-gradient(45deg, rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5), rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.75)`;
+    card.appendChild(closeBtn);
+
+    card.style.background = book.style;
 
     return card;
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '1937');
-addBookToLibrary('Watership Down', 'Richard Adams', '1972');
-addBookToLibrary('Dune', 'Frank Herbert', '1965');
-outputBooks();
